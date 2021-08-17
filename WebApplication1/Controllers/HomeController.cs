89bb7ab401567ce8fd0215ecae6efadc5eca31cd
kamandar0047 <82367116+kamandar0047@users.noreply.github.com>
2021-08-17 22:17:54 +1100

    kamandar0047 / aspnetbegin

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //ViewBag.Name = "adam";
+            //ViewBag.Surname = "saasa";
+            //TempData["Age"] = 25;
+            //return RedirectToAction(nameof(Test));
+            string fullname = "Ehmed Memmed";
+            List<int> numbers = new List<int> { 1,2,3,4,5};
+            List<Student> students = new List<Student>
+            {
+                 new Student{Id=1,Name="kama",Surname="muro"},
+                new Student{Id=2,Name="ada",Surname="muaaro"},
+                new Student{Id=3,Name="assda",Surname="mussaaro"}
+            };
+
+
+            HomeViewModel model = new HomeViewModel
+            {
+
+                Students=students,
+                Numbers = numbers,
+                Fullname = fullname,
+
+
+            };
+            return View(model);
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Fullname{get; set; }
+        public List<int> Numbers { get; set; }
+
+
+        public List<Student> Students { get; set; }
+    }
+}
